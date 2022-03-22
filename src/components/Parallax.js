4e3d17759fce3ddb95bfab7cee8d799967893034
appitionitsolution <applicationitsolution@gmail.com>
2022-03-22 16:12:52 +0200

    test

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react"
 import AOS from "aos"
 
 import "aos/dist/aos.css"
+
 // import "../scss/style.scss"
 
 const Parallax = ({ position = "left", children }) => {
@@ -10,9 +11,14 @@ const Parallax = ({ position = "left", children }) => {
       easing: "ease-out-cubic",
       once: true,
       offset: 50,
+      delay: 50,
     })
   }, [])
 
-  return <div data-aos={`fade-${position}`}>{children}</div>
+  return (
+    <div data-aos={`fade-${position}`} data-aos-anchor-placement="top-bottom">
+      {children}
+    </div>
+  )
 }
 export default Parallax
